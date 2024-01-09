88aec68832e48025becf7b106337dcba453bf802
Pettinari Gabriel <118709375+GabrielPetty@users.noreply.github.com>
2024-01-09 14:53:23 -0300

    Update conexion.php

--- config/conexion.php
+++ config/conexion.php
@@ -7,20 +7,7 @@
 
   $conn = new mysqli($hostname, $username, $passname, $dbname);
   
-/*
-  if (mysqli_connect_errno()){
-    printf('CONEXION FALLID ...', mysqli_connect_error());
-    exit();
-  }echo 'CONEXION DB.'
-
-*/
-
 if ($conn->connect_error){
   die('ERROR CONECTION...'. $conn->connect_error);
-
-}/*else{
-  echo 'DB. CONNECT -';
-}*/
-
 
 ?>
